8fec21c27777dc069c425e45d245d54effb6c348
Riddhiburadkar-28 <riddhiburadkar28@gmail.com>
2026-03-31 22:30:00 +0530

    Unit 3 Q8 - TextProcessingBuilder and comparison with StringBuffer

---
+++ Q8 _TextProcessingBuilder.java
@@ -0,0 +1,15 @@
+class TextProcessingBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+
+        sb.append(" Programming");
+
+        System.out.println("Modified String: " + sb);
+    }
+}
+
+/*
+Difference:
+StringBuffer → Thread-safe (synchronized), slower
+StringBuilder → Not thread-safe, faster
+*/
