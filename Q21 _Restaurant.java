8fedba5708c4eb62215b141d3e250f44899a7607
Riddhiburadkar-28 <riddhiburadkar28@gmail.com>
2026-03-31 23:06:30 +0530

    Unit4 Q21 - Restaurant producer-consumer program

---
+++ Q21 _Restaurant.java
@@ -0,0 +1,28 @@
+class Restaurant {
+    boolean foodReady = false;
+
+    synchronized void prepareFood() {
+        System.out.println("Chef prepared food");
+        foodReady = true;
+        notify();
+    }
+
+    synchronized void serveFood() {
+        try {
+            while (!foodReady) {
+                wait();
+            }
+            System.out.println("Waiter served food");
+        } catch (InterruptedException e) {}
+    }
+
+    public static void main(String[] args) {
+        Restaurant r = new Restaurant();
+
+        Thread chef = new Thread(() -> r.prepareFood());
+        Thread waiter = new Thread(() -> r.serveFood());
+
+        waiter.start();
+        chef.start();
+    }
+}
